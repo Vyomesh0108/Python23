b19f9addb936a50b9cb7620616bedb75675e4a0e
vyomeshmistry <vyomeshmistry108@gmail.com>
2023-11-03 18:29:31 -0400

    Usage of flush parameter in print() function

---
+++ FlushInPrint.py
@@ -0,0 +1,11 @@
+# Flush in print() function
+
+import time
+
+count_seconds = 3
+for i in reversed(range(count_seconds + 1)):
+    if i > 0:
+        print(i, end=">>>", flush=True)
+        time.sleep(3)
+    else:
+        print('Start')
